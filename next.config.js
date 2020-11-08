63d1b59498f1e63eced1cf3e88f61e5c743fa1ed
febriliankr <febrilian.kr@gmail.com>
2020-11-09 00:08:35 +0700

    lowering image res

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
 module.exports = {
   // Target must be serverless
   target: "serverless",
