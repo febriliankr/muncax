df6de0f11e2fb801172b0a0e673b5a6809dc2679
febriliankr <febrilian.kr@gmail.com>
2020-11-10 02:20:49 +0700

    added suggestion

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,6 +5,7 @@ function Search({}) {
   const router = useRouter();
   const [currentSearch, setCurrentSearch] = useState(router.query.q);
   const [search, setSearch] = useState(router.query?.q);
+  const [searchFocus, setSearchFocus] = useState(false);
   function handleSubmit(e) {
     e.preventDefault();
     console.log(search);
@@ -12,20 +13,58 @@ function Search({}) {
     router.push(`/search?st=product&q=${search}`);
   }
 
+  function handleClickSuggestion(e, query) {
+    e.preventDefault();
+    // console.log('e,', e,)
+    router.push(`/search?st=product&q=${query}`);
+  }
+
+  function handleChange(e) {
+    setSearch(e.target.value);
+  }
+
+  const [suggestions, setSuggestions] = useState([
+    "Carrier Gunung",
+    "Tenda",
+    "Lampu",
+    "Sandal",
+    "Sepatu Gunung",
+  ]);
+
   return (
     <div className="searchbar__container">
       <form onSubmit={handleSubmit} className="search-form">
         <input
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={handleChange}
           type="text"
           className="searchTerm"
           placeholder="Cari barang yang anda butuhkan"
           value={search}
+          onFocus={() => setSearchFocus(true)}
+          onBlur={() => setSearchFocus(false)}
         />
-        <button type="submit" className="searchButton">
+
+        <button onClick={handleSubmit} className="searchButton">
           <AiOutlineSearch />
         </button>
       </form>
+      {searchFocus ? (
+        <div className="searchbar__suggestions">
+          <ul>
+            {suggestions.map((text, index) => {
+              return (
+                <li
+                  onMouseOver={() => setSearch(text)}
+                  // onClick={(e) => alert(e)}
+                  key={index}
+                >
+                  {text}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      ) : null}
     </div>
   );
 }
